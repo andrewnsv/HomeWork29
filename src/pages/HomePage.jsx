7c345208d352c8ab945a523ef61c703ba971ba41
andrewnsv <andrejnizovoj598@gmail.com>
2023-05-01 13:10:34 +0300

    fix homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const HomePage = () => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <h1>Hello on home page</h1>
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Typography sx={{ mt: 4 }} variant="h2" component="h1">
+        It`s home page
+      </Typography>
     </Box>
   );
 };
